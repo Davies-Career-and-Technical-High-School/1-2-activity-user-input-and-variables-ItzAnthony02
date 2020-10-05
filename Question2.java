0775d53a168228c87a2d68e73791d75bd673fd77
Anthony <anthony.reyes@daviestech.org>
2020-10-05 22:29:59 +0000

    I completed the work

--- Question2.java
+++ Question2.java
@@ -1,11 +1,24 @@
 /* Activity 1.2 - Question 2 */
-
+import java.util.Scanner;
 
 
 class Question2 {
     public static void main(String[] args) {
 
         /* Write your code here */
+      Scanner scan = new Scanner(System.in); //this is the delacred variable 
+    
+    String name1;
+    String name2;
+    String name3;
 
+    System.out.println("Please enter three names.");
+    
+    name1 = scan.nextLine(); //user input
+    name2 = scan.nextLine(); //user input
+    name3 = scan.nextLine(); //user input
+
+    System.out.println(name1 + " "+ name2 + " "+ name3 );
+    
     }
 }
